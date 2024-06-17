0505ad6c3a03e05b8595a8bec3799100a0c47d47
Whispering Wind <151555003+ReturnFI@users.noreply.github.com>
2024-06-17 08:55:41 +0330

    Update URI

--- menu.sh
+++ menu.sh
@@ -111,8 +111,8 @@ show_uri() {
                     IP6=$(curl -s -6 ip.sb)
 
                     # Construct URI
-                    URI="hy2://$username:$authpassword@$IP:$port?obfs=salamander&obfs-password=$obfspassword&pinSHA256=$sha256&insecure=1&sni=bts.com#$username-IPv4"
-                    URI6="hy2://$username:$authpassword@[$IP6]:$port?obfs=salamander&obfs-password=$obfspassword&pinSHA256=$sha256&insecure=1&sni=bts.com#$username-IPv6"
+                    URI="hy2://$username%3A$authpassword@$IP:$port?obfs=salamander&obfs-password=$obfspassword&pinSHA256=$sha256&insecure=1&sni=bts.com#$username-IPv4"
+                    URI6="hy2://$username%3A$authpassword@[$IP6]:$port?obfs=salamander&obfs-password=$obfspassword&pinSHA256=$sha256&insecure=1&sni=bts.com#$username-IPv6"
 
                     cols=$(tput cols)
                     rows=$(tput lines)

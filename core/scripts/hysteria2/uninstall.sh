b13ee6c335ef4c0b016dc7e709d703708b51b10d
Sarina <iam54r1n40@proton.me>
2024-07-23 17:35:01 +0330

    Remove dealy from uninstall.sh

--- core/scripts/hysteria2/uninstall.sh
+++ core/scripts/hysteria2/uninstall.sh
@@ -1,28 +1,28 @@
 source /etc/hysteria/core/scripts/path.sh
 
 echo "Uninstalling Hysteria2..."
-sleep 1
+#sleep 1
 echo "Running uninstallation script..."
 bash <(curl -fsSL https://get.hy2.sh/) --remove >/dev/null 2>&1
-sleep 1
+#sleep 1
 echo "Removing WARP"
 python3 $CLI_PATH uninstall-warp
 echo "Removing Hysteria folder..."
 rm -rf /etc/hysteria >/dev/null 2>&1
-sleep 1
+#sleep 1
 echo "Deleting hysteria user..."
 userdel -r hysteria >/dev/null 2>&1
-sleep 1
+#sleep 1
 echo "Removing systemd service files..."
 rm -f /etc/systemd/system/multi-user.target.wants/hysteria-server.service >/dev/null 2>&1
 rm -f /etc/systemd/system/multi-user.target.wants/hysteria-server@*.service >/dev/null 2>&1
-sleep 1
+#sleep 1
 echo "Reloading systemd daemon..."
 systemctl daemon-reload >/dev/null 2>&1
-sleep 1
+#sleep 1
 echo "Removing cron jobs..."
 (crontab -l | grep -v "python3 /etc/hysteria/core/cli.py traffic-status" | crontab -) >/dev/null 2>&1
 (crontab -l | grep -v "/etc/hysteria/core/scripts/hysteria2/kick.sh" | crontab -) >/dev/null 2>&1
-sleep 1
+#sleep 1
 echo "Hysteria2 uninstalled!"
 echo ""

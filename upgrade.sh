75d3e9cd2e15c9e655109be6d448a6d7e690f7c3
Whispering Wind <151555003+ReturnFI@users.noreply.github.com>
2024-08-14 10:47:27 +0330

    Update

--- upgrade.sh
+++ upgrade.sh
@@ -38,14 +38,19 @@ chown hysteria:hysteria /etc/hysteria/ca.key /etc/hysteria/ca.crt
 chmod 640 /etc/hysteria/ca.key /etc/hysteria/ca.crt
 chown -R hysteria:hysteria /etc/hysteria/core/scripts/singbox
 
+echo "Setting execute permissions for user.sh and kick.sh"
+chmod +x /etc/hysteria/core/scripts/hysteria2/user.sh
+chmod +x /etc/hysteria/core/scripts/hysteria2/kick.sh
+
+cd /etc/hysteria
+python3 -m venv hysteria2_venv
+source /etc/hysteria/hysteria2_venv/bin/activate
+pip install -r requirements.txt
+
 echo "Restarting hysteria services"
 systemctl restart hysteria-server.service
 systemctl restart hysteria-bot.service
 systemctl restart singbox.service
-
-echo "Setting execute permissions for user.sh and kick.sh"
-chmod +x /etc/hysteria/core/scripts/hysteria2/user.sh
-chmod +x /etc/hysteria/core/scripts/hysteria2/kick.sh
 
 echo "Checking hysteria-server.service status"
 if systemctl is-active --quiet hysteria-server.service; then
@@ -53,11 +58,6 @@ if systemctl is-active --quiet hysteria-server.service; then
 else
     echo "Upgrade failed: hysteria-server.service is not active"
 fi
-
-cd /etc/hysteria
-python3 -m venv hysteria2_venv
-source /etc/hysteria/hysteria2_venv/bin/activate
-pip install -r requirements.txt
 
 chmod +x menu.sh
 ./menu.sh

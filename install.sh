67db6a64919f3ade889d506e9accc3172254097e
Whispering Wind <151555003+ReturnFI@users.noreply.github.com>
2024-09-02 23:46:40 +0330

    Update Packages

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
-# Ensure necessary packages are installed
-# Check if the script is being run by the root user
+#!/bin/bash
+
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run as root."
     exit 1
 fi
 
 clear
-if ! command -v jq &> /dev/null || ! command -v git &> /dev/null || ! command -v qrencode &> /dev/null || ! command -v curl &> /dev/null; then
-    echo "${yellow}Necessary packages are not installed. Please wait while they are being installed..."
-    sleep 3
-    echo 
-    apt update && apt upgrade -y && apt install jq qrencode curl pwgen uuid-runtime python3 python3-pip python3-venv git bc zip -y
+REQUIRED_PACKAGES="jq qrencode curl pwgen uuid-runtime python3 python3-pip python3-venv git bc zip"
+
+MISSING_PACKAGES=$(dpkg-query -W -f='${Package}\n' $REQUIRED_PACKAGES 2>&1 | grep -v "ok installed")
+if [ -n "$MISSING_PACKAGES" ]; then
+    echo "The following packages are missing and will be installed: $MISSING_PACKAGES"
+    apt update
+    apt install -y $MISSING_PACKAGES
 fi
 
 git clone https://github.com/ReturnFI/Hysteria2 /etc/hysteria
 
-# Create and activate Python virtual environment
 cd /etc/hysteria
 python3 -m venv hysteria2_venv
 source /etc/hysteria/hysteria2_venv/bin/activate
 pip install -r requirements.txt
 
-# Add alias 'hys2' for Hysteria2
 if ! grep -q "alias hys2='source /etc/hysteria/hysteria2_venv/bin/activate && /etc/hysteria/menu.sh'" ~/.bashrc; then
     echo "alias hys2='source /etc/hysteria/hysteria2_venv/bin/activate && /etc/hysteria/menu.sh'" >> ~/.bashrc
     source ~/.bashrc

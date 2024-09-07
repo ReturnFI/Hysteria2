c80c1c76e48af82adf4b7d6f7a15c9234394cdc1
Whispering Wind <151555003+ReturnFI@users.noreply.github.com>
2024-09-07 21:54:06 +0330

    Fix Kick Users

--- core/scripts/hysteria2/kick.sh
+++ core/scripts/hysteria2/kick.sh
@@ -12,17 +12,26 @@ kick_user() {
 SECRET=$(jq -r '.trafficStats.secret' "$CONFIG_FILE")
 
 for USERNAME in $(jq -r 'keys[]' "$USERS_FILE"); do
-  MAX_DOWNLOAD_BYTES=$(jq -r --arg user "$USERNAME" '.[$user].max_download_bytes' "$USERS_FILE")
-  EXPIRATION_DAYS=$(jq -r --arg user "$USERNAME" '.[$user].expiration_days' "$USERS_FILE")
+  MAX_DOWNLOAD_BYTES=$(jq -r --arg user "$USERNAME" '.[$user].max_download_bytes // 0' "$USERS_FILE")
+  EXPIRATION_DAYS=$(jq -r --arg user "$USERNAME" '.[$user].expiration_days // 0' "$USERS_FILE")
   ACCOUNT_CREATION_DATE=$(jq -r --arg user "$USERNAME" '.[$user].account_creation_date' "$USERS_FILE")
-  CURRENT_DOWNLOAD_BYTES=$(jq -r --arg user "$USERNAME" '.[$user].download_bytes' "$USERS_FILE")
-  BLOCKED=$(jq -r --arg user "$USERNAME" '.[$user].blocked' "$USERS_FILE")
+  CURRENT_DOWNLOAD_BYTES=$(jq -r --arg user "$USERNAME" '.[$user].download_bytes // 0' "$USERS_FILE")
+  BLOCKED=$(jq -r --arg user "$USERNAME" '.[$user].blocked // false' "$USERS_FILE")
+
+  if [ -z "$ACCOUNT_CREATION_DATE" ]; then
+    echo "Skipping $USERNAME due to missing account creation date."
+    continue
+  fi
 
   CURRENT_DATE=$(date +%s)
   EXPIRATION_DATE=$(date -d "$ACCOUNT_CREATION_DATE + $EXPIRATION_DAYS days" +%s)
 
-  if [ "$CURRENT_DOWNLOAD_BYTES" -ge "$MAX_DOWNLOAD_BYTES" ] || [ "$CURRENT_DATE" -ge "$EXPIRATION_DATE" ]; then
-    jq --arg user "$USERNAME" '.[$user].blocked = true' "$USERS_FILE" > temp.json && mv temp.json "$USERS_FILE"
-    kick_user "$USERNAME" "$SECRET"
+  if [ "$MAX_DOWNLOAD_BYTES" -gt 0 ] && [ "$CURRENT_DOWNLOAD_BYTES" -ge 0 ] && [ "$EXPIRATION_DAYS" -gt 0 ]; then
+    if [ "$CURRENT_DOWNLOAD_BYTES" -ge "$MAX_DOWNLOAD_BYTES" ] || [ "$CURRENT_DATE" -ge "$EXPIRATION_DATE" ]; then
+      jq --arg user "$USERNAME" '.[$user].blocked = true' "$USERS_FILE" > temp.json && mv temp.json "$USERS_FILE"
+      kick_user "$USERNAME" "$SECRET"
+    fi
+  else
+    echo "Skipping $USERNAME due to invalid or missing data."
   fi
 done

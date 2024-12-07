867ad97d50d61e03192f35f3106f66915c7fbc39
Whispering Wind <151555003+ReturnFI@users.noreply.github.com>
2024-12-08 00:17:42 +0330

    Fix cron jobs

--- core/scripts/hysteria2/uninstall.sh
+++ core/scripts/hysteria2/uninstall.sh
@@ -24,6 +24,9 @@ systemctl daemon-reload >/dev/null 2>&1
 echo "Removing cron jobs..."
 (crontab -l | grep -v "python3 /etc/hysteria/core/cli.py traffic-status" | crontab -) >/dev/null 2>&1
 (crontab -l | grep -v "/etc/hysteria/core/scripts/hysteria2/kick.sh" | crontab -) >/dev/null 2>&1
+(crontab -l | grep -v "python3 /etc/hysteria/core/cli.py restart-hysteria2" | crontab -) >/dev/null 2>&1
+(crontab -l | grep -v "python3 /etc/hysteria/core/cli.py backup-hysteria" | crontab -) >/dev/null 2>&1
+
 
 echo "Removing alias 'hys2' from .bashrc..."
 sed -i '/alias hys2=.*\/etc\/hysteria\/menu.sh/d' ~/.bashrc

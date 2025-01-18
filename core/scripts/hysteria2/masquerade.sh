01ef6d65d053b8b7cd1411d280e576f462ea6e77
Whispering Wind <151555003+ReturnFI@users.noreply.github.com>
2025-01-18 23:38:45 +0330

    Accurately check

--- core/scripts/hysteria2/masquerade.sh
+++ core/scripts/hysteria2/masquerade.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
 source /etc/hysteria/core/scripts/path.sh
 
 function is_masquerade_enabled() {
-    if jq -e '.masquerade' $CONFIG_FILE > /dev/null; then
-        echo "Masquerade is already enabled."
-        exit 0
-    else
-        echo "Masquerade is not enabled."
-    fi
+    jq -e '.masquerade' $CONFIG_FILE > /dev/null 2>&1
 }
 
 function enable_masquerade() {
+    if is_masquerade_enabled; then
+        echo "Masquerade is already enabled."
+        exit 0
+    fi
     url="https://$1"
     jq --arg url "$url" '. + {masquerade: {type: "proxy", proxy: {url: $url, rewriteHost: true}, listenHTTP: ":80", listenHTTPS: ":443", forceHTTPS: true}}' $CONFIG_FILE > tmp.json && mv tmp.json $CONFIG_FILE
     echo "Masquerade enabled with URL: $url"
@@ -19,16 +17,18 @@ function enable_masquerade() {
 }
 
 function remove_masquerade() {
+    if ! is_masquerade_enabled; then
+        echo "Masquerade is not enabled."
+        exit 0
+    fi
     jq 'del(.masquerade)' $CONFIG_FILE > tmp.json && mv tmp.json $CONFIG_FILE
     echo "Masquerade removed from config.json"
     python3 "$CLI_PATH" restart-hysteria2 > /dev/null 2>&1
 }
 
 if [[ "$1" == "1" ]]; then
-    is_masquerade_enabled
     enable_masquerade "$2"
 elif [[ "$1" == "2" ]]; then
-    is_masquerade_enabled
     remove_masquerade
 else
     echo "Usage: $0 {1|2} [domain]"

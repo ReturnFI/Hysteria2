4f3c9b180faceab0fb01203fdd7594b8cd10bf7e
Iam54r1n4 <iam54r1n40@proton.me>
2025-01-26 12:11:30 +0000

    Include telegram|normalsub|singbox endpoints

--- core/scripts/webpanel/routers/api/v1/config/__init__.py
+++ core/scripts/webpanel/routers/api/v1/config/__init__.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter
 from . import hysteria
 from . import warp
+from . import telegram
+from . import normalsub
+from . import singbox
 
 router = APIRouter()
 
 
 router.include_router(hysteria.router, prefix='/hysteria')
 router.include_router(warp.router, prefix='/warp')
+router.include_router(telegram.router, prefix='/telegram')
+router.include_router(normalsub.router, prefix='/normalsub')
+router.include_router(singbox.router, prefix='/singbox')

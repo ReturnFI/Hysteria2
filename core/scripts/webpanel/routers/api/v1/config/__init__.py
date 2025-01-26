99d0ec9b0fabc947525f5e12b8b8a5ca4e187d00
Iam54r1n4 <iam54r1n40@proton.me>
2025-01-26 13:17:12 +0000

    Add api/v1/config/install-tcp-brutal|update-geo endpoints

--- core/scripts/webpanel/routers/api/v1/config/__init__.py
+++ core/scripts/webpanel/routers/api/v1/config/__init__.py
@@ -4,6 +4,7 @@ from . import warp
 from . import telegram
 from . import normalsub
 from . import singbox
+from . import misc
 
 router = APIRouter()
 
@@ -13,3 +14,4 @@ router.include_router(warp.router, prefix='/warp')
 router.include_router(telegram.router, prefix='/telegram')
 router.include_router(normalsub.router, prefix='/normalsub')
 router.include_router(singbox.router, prefix='/singbox')
+router.include_router(misc.router)

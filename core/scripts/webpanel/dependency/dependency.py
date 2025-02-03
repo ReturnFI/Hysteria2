9d42e56f47d1c11ac6bfceb279388c8e85a2b7fe
Iam54r1n4 <iam54r1n40@proton.me>
2025-02-03 19:03:54 +0000

    Implement some dependencies available across endpoints

---
+++ core/scripts/webpanel/dependency/dependency.py
@@ -0,0 +1,18 @@
+from fastapi.templating import Jinja2Templates
+
+from authentication.session import SessionStorage, SessionManager
+from config import CONFIGS
+
+__TEMPLATES = Jinja2Templates(directory='templates')
+
+
+def get_templates() -> Jinja2Templates:
+    return __TEMPLATES
+
+
+__SESSION_STORAGE = SessionStorage()
+__SESSION_MANAGER = SessionManager(__SESSION_STORAGE, CONFIGS.EXPIRATION_MINUTES)
+
+
+def get_session_manager() -> SessionManager:
+    return __SESSION_MANAGER

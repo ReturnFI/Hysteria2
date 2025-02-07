f0fbaa2774ee31c4690226aca33645692247a842
Iam54r1n4 <iam54r1n40@proton.me>
2025-02-07 18:26:45 +0000

    Add status.sh script to get status of project services

---
+++ core/scripts/status.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -a services=(
+    "hysteria-server.service"
+    "webpanel.service"
+    "hysteria-bot.service"
+    "normalsub.service"
+    "singbox.service"
+    "wg-quick@wgcf.service"
+)
+
+status_json="{"
+for service in "${services[@]}"; do
+    if systemctl is-active --quiet "$service"; then
+        status_json+="\"$service\":true,"
+    else
+        status_json+="\"$service\":false,"
+    fi
+done
+
+# Remove trailing comma and close JSON properly
+status_json="${status_json%,}}"
+
+# Format output as valid JSON
+echo "$status_json" | jq -M .

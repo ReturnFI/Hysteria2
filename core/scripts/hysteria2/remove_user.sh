7aab7b5489548d8fa1f5b8006ebcba8bfba4c39d
Whispering Wind <151555003+ReturnFI@users.noreply.github.com>
2025-03-27 20:29:34 +0330

    HotFix: Restart service to clear API traffic

--- core/scripts/hysteria2/remove_user.sh
+++ core/scripts/hysteria2/remove_user.sh
@@ -15,7 +15,7 @@ remove_user() {
     if [ -f "$USERS_FILE" ]; then
         if jq -e "has(\"$username\")" "$USERS_FILE" > /dev/null; then
             jq --arg username "$username" 'del(.[$username])' "$USERS_FILE" > "${USERS_FILE}.temp" && mv "${USERS_FILE}.temp" "$USERS_FILE"
-            
+            python3 $CLI_PATH restart-hysteria2
             echo "User $username removed successfully."
         else
             echo -e "${red}Error:${NC} User $username not found."
